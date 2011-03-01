37436aeac0089aac9f855c17f2fb2cab7e44d604
Margus Freudenthal <margus@cyber.ee>
2011-03-01 19:31:23 +0200

    Better indent.

--- src/tool/OberonGenerator.scala
+++ src/tool/OberonGenerator.scala
@@ -2,22 +2,22 @@ package ee.cyber.simplicitas.oberonexample;
 
 import ee.cyber.simplicitas.{GeneratorBase, MainBase, PrettyPrint}
 
-class OberonGenerator(destDir: String) 
+class OberonGenerator(destDir: String)
         extends GeneratorBase(destDir) {
-  def generate(tree: Module) {
-    println(PrettyPrint.prettyPrint(tree))
-  }
-}
-  
-object OberonMain extends MainBase {
-  def main(argv: Array[String]) {
-    parseOptions(argv)
-    val grammar = new OberonGrammar()
-    for (arg <- sources) {
-      grammar.parseFile(arg)
-      checkErrors(grammar.errors)
-      
-      new OberonGenerator(destDir).generate(grammar.tree)        
+    def generate(tree: Module) {
+        println(PrettyPrint.prettyPrint(tree))
     }
-  }
+}
+
+object OberonMain extends MainBase {
+    def main(argv: Array[String]) {
+        parseOptions(argv)
+        val grammar = new OberonGrammar()
+        for (arg <- sources) {
+            grammar.parseFile(arg)
+            checkErrors(grammar.errors)
+
+            new OberonGenerator(destDir).generate(grammar.tree)
+        }
+    }
 }
