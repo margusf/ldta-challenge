ce48a9d7940fdb198940d9fa44b85eeec39b98f1
Margus Freudenthal <margus@cyber.ee>
2011-05-21 19:31:02 +0300

    Beginnings of formal parameters.

--- src/tool/Codegen.scala
+++ src/tool/Codegen.scala
@@ -1,29 +1,61 @@
 package ee.cyber.simplicitas.oberonexample
 
 import gen.Gen
+import collection.mutable.ArrayBuffer
 
 object Codegen {
     def generate(module: Module) {
-        val code = generateModule(module)
+        val ctx = new GenCtx
+        generateModule(ctx, module)
 
-        println(code)
+        println(ctx.topLevel)
     }
 
-    private def generateModule(module: Module): Gen = {
-        println(generateDecl(module.decl))
+    private def generateModule(ctx: GenCtx, module: Module) {
+        println(generateDecl(ctx, module.decl))
 
         val stmt = generateStatement(module.statements)
         println(stmt)
 
         gen.Module(module.name1.text, null, null, null)
+    }
+
+    private def generateDecl(ctx: GenCtx, decl: Declarations): Gen = {
+        // TODO: create list of vars.
+        for (proc <- decl.procedures) {
+            val (params, pTypes) = getParameters(proc)
+
+            // TODO: generate procedure.
+            // TODO: add procedure to toplevel.
+            ctx.addToplevel(
+                gen.ProcDecl(proc.name.text, params, pTypes, List.empty))
+        }
+
         null
     }
 
-    private def generateDecl(decl: Declarations): Gen = {
-        null
+    private def getParameters(proc: ProcedureDecl) = {
+        val params = new ArrayBuffer[String]
+        val pTypes = new ArrayBuffer[String]
+
+        for (fp <- proc.params; p <- fp.ids.ids) {
+            // TODO: deal with var parameters
+            params += p.text
+            // TODO: use actual type instead of type name.
+            pTypes += fp.pType.text
+        }
+        (params.toList, pTypes.toList)
     }
 
     private def generateStatement(stmt: StatementSequence): Gen = {
         null
+    }
+}
+
+class GenCtx {
+    val topLevel = new ArrayBuffer[gen.ProcDecl]
+
+    def addToplevel(proc: gen.ProcDecl) {
+        topLevel += proc
     }
 }
