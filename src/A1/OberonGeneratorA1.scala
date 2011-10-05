a22c911c557a482dd7deb461ca95030f5f8c5689
Margus Freudenthal <margus@cyber.ee>
2011-10-05 18:12:26 +0300

    Create a log file.

--- src/A1/OberonGeneratorA1.scala
+++ src/A1/OberonGeneratorA1.scala
@@ -1,8 +1,18 @@
 package ee.cyber.simplicitas.oberonexample
 
-import ee.cyber.simplicitas.MainBase
+import ee.cyber.simplicitas.{SourceMessage, MainBase}
 
 object OberonMainA1 extends MainBase {
+    def logErrors(fileName: String, errors: Seq[SourceMessage]) {
+        if (!errors.isEmpty) {
+            var pw = new java.io.PrintWriter(
+                    new java.io.FileWriter("/tmp/oberon-log.txt", true), true)
+            pw.println("\nFile: " + fileName)
+            errors foreach (pw.println)
+            pw.close()
+        }
+    }
+
     def main(argv: Array[String]) {
         parseOptions(argv)
         val grammar = new ast.OberonGrammar()
@@ -10,10 +20,12 @@ object OberonMainA1 extends MainBase {
             grammar.parseFile(arg)
             if (!grammar.errors.isEmpty) {
                 println("parse failed")
+                logErrors(arg, grammar.errors)
             } else {
                 val otherErrors = OtherChecks.process(grammar.tree)
                 if (!otherErrors.isEmpty) {
                     println("parse failed")
+                    logErrors(arg, otherErrors)
                 } else {
                     val nameErrors = NameBindingA1.process(grammar.tree)
                     checkErrors(nameErrors)
