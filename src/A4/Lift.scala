58a9a7f82c6f15683ad5bd2df81254fea93b125a
Margus Freudenthal <margus@cyber.ee>
2011-12-06 13:20:57 +0200

    Also lift types.

--- src/A4/Lift.scala
+++ src/A4/Lift.scala
@@ -16,21 +16,29 @@ class Lift(module: Module) {
     // List of top-level procedures (will be filled when procedure
     // declarations are flattened)
     val topLevel = ArrayBuffer[ProcedureDecl]()
+    val topTypes = ArrayBuffer[TypeDef]()
     val topIds = collection.mutable.Set[String]()
 
     // TODO: also lift types!
     def apply() {
+        module.decl.types.foreach(doType(null))
         module.decl.procedures.foreach(doProcedure(null))
+
         module.decl.procedures = topLevel.toList
-        module.walkTree(fixProcedureCall)
+        module.decl.types = topTypes.toList
+
+        module.walkTree(fixReferences)
     }
 
     private def doProcedure(prefix: String)(proc: ProcedureDecl) {
         val myNewName = newLiftedName(prefix, proc.name.text)
 
+        proc.decl.types.foreach(doType(myNewName))
         proc.decl.procedures.foreach(doProcedure(myNewName))
-        // All the child procedures are removed.
+
+        // All the child procedures and types are removed.
         proc.decl.procedures = Nil
+        proc.decl.types = Nil
 
         // Add main procedure after children. This preserves the scopes.
         proc.name.text = myNewName
@@ -39,9 +47,20 @@ class Lift(module: Module) {
 
     }
 
+    private def doType(prefix: String)(td: TypeDef) {
+        val newTypeName = newLiftedName(prefix, td.name.text)
+        td.name.text = newTypeName
+        addToTop(td)
+    }
+
     private def addToTop(proc: ProcedureDecl) {
         topLevel += proc
         topIds += proc.name.text
+    }
+
+    private def addToTop(td: TypeDef) {
+        topTypes += td
+        topIds += td.name.text
     }
 
     private def newLiftedName(prefix: String, name: String) = {
@@ -62,11 +81,33 @@ class Lift(module: Module) {
             makeUnique(prefix + name.capitalize)
     }
 
-    private def fixProcedureCall(node: CommonNode) {
+    private def fixReferences(node: CommonNode) {
+//        def fix(id: Id) {
+//            val original = id.ref.asInstanceOf[Id]
+//            if (original ne null) {
+//                println("renaming: " + id.text + " -> " + original.text)
+//                id.text = original.text
+//            } else {
+//                println("No ref: " + id.text)
+//            }
+//        }
+//
         node match {
-            case call @ ProcedureCall(name, _) =>
-                val original = name.ref.asInstanceOf[Id]
-                name.text = original.text
+//            case call @ ProcedureCall(name, _) =>
+//                fix(name)
+//            case VarDef(_, id @ Id(_)) =>
+//                fix(id)
+//            case TypeDef(_, id @ Id(_)) =>
+//                fix(id)
+//            case FormalParam(_, _, id @ Id(_)) =>
+//                fix(id)
+//            case ArrayType(_, id @ Id(_)) =>
+//                fix(id)
+            case id @ Id(_) =>
+                val original = id.ref.asInstanceOf[Id]
+                if ((original ne null) && (original.text != id.text)) {
+                    id.text = original.text
+                }
             case _ =>
                 ()
         }
