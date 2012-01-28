c17759f14c082e74affd0b09e7f9f6d516b58df1
Margus Freudenthal <margus@cyber.ee>
2012-01-28 22:51:43 +0200

    Refactored the A1 to use better main program.

--- src/A1/OberonGeneratorA1.scala
+++ src/A1/OberonGeneratorA1.scala
@@ -3,39 +3,18 @@ package ee.cyber.simplicitas.oberonexample
 import ee.cyber.simplicitas.{SourceMessage, MainBase}
 
 object OberonMainA1 extends MainBase {
-    def logErrors(fileName: String, errors: Seq[SourceMessage]) {
-        if (!errors.isEmpty) {
-            var pw = new java.io.PrintWriter(
-                    new java.io.FileWriter("/tmp/oberon-log.txt", true), true)
-            pw.println("\nFile: " + fileName)
-            errors foreach (pw.println)
-            pw.close()
-        }
+    val grammar = new ast.OberonGrammar()
+
+    def processFile(file: String) {
+        Errors.parse(grammar, file)
+        OtherChecks.process(grammar.tree)
+        NameBindingA1.process(grammar.tree)
     }
 
     def main(argv: Array[String]) {
         parseOptions(argv)
-        val grammar = new ast.OberonGrammar()
         for (arg <- sources) {
-            grammar.parseFile(arg)
-            if (!grammar.errors.isEmpty) {
-                println("parse failed")
-                logErrors(arg, grammar.errors)
-            } else {
-                val otherErrors = OtherChecks.process(grammar.tree)
-                if (!otherErrors.isEmpty) {
-                    println("parse failed")
-                    logErrors(arg, otherErrors)
-                } else {
-                    NameBindingA1.process(grammar.tree) match {
-                        case Some(msg) =>
-                            println("line: " + msg.startLine +
-                                    "  " + msg.message)
-                        case None =>
-                            ()
-                    }
-                }
-            }
+            Errors.handle(() => processFile(arg))
         }
     }
 }
